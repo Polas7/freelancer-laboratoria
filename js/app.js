12294ec74621b3ee76beb771f4201de0af7e4a0a
Pola <Pola.kinomoto@gmail.com>
2017-06-01 23:02:55 -0400

    formulario

--- js/app.js
+++ js/app.js
@@ -1,3 +1,25 @@
-/*
- * Archivo principal de JS
- */
+/*validacion del formulario*/
+var boton = document.getElementById("boton");
+boton.addEventListener('click', validar);
+
+ function validar() {
+ 	var name = document.getElementById("name");
+ 	var email = document.getElementById("email");
+ 	var phoneNumber = document.getElementById("phone");
+ 	var message = document.getElementById("textarea");
+ 	var contenido = "";
+
+    // Si los value vienen vacios envio mensaje
+    if (name.value === "") {
+    	name.value = "● Please enter your name.";
+    }
+    if (email.value === "") {
+    	email.value = "● Please enter your email.";
+    }
+    if (phone.value === "") {
+    	phone.value = "● Please enter your phone number.";
+    }
+    if (mensaje.value === "") {
+    	mensaje.value = "● Please enter your message.";
+    }
+};
